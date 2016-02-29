830886b70276e1b1ac06b9ce8c2decad8a4bd788
nknorr <nicolas.knorr@gmail.com>
2016-02-29 15:06:33 +0100

    Ajout du Log

---
+++ src/DistributedSystems/Log.java
@@ -0,0 +1,53 @@
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.util.List;
+
+
+public class Log {
+	String fichier;
+
+	public String getFichier() {
+		return fichier;
+	}
+
+	public void setFichier(String fichier) {
+		this.fichier = fichier;
+	}
+
+	public Log(String fichier) {
+		super();
+		this.fichier = fichier;
+	}
+	
+	public void WriteLog(String path, String data)
+	{
+		try {
+
+			String content = "This is the content to write into file";
+
+			File file = new File(path);
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(data);
+			bw.close();
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
